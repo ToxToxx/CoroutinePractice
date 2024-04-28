0dde573c3c25ca542a9d589736affe1f6895f586
Matvey <sinema3d00@gmail.com>
2024-04-28 22:40:00 +0300

    practicing coroutine with UI exercise

---
+++ Assets/Scripts/HealthView.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthView : MonoBehaviour
+{
+    [SerializeField] private float _smoothDecreaseDuration = 0.5f;
+}
